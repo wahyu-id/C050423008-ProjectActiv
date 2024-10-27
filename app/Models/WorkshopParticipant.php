9b863d77d660b857646342dd693c7396b92231de
wahyu-nurhadi0915 <muhammadwahyunurhadi@gmail.com>
2024-10-27 22:44:49 +0800

    first commit

---
+++ app/Models/WorkshopParticipant.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class WorkshopParticipant extends Model
+{
+    use HasFactory, softDeletes;
+
+    protected $table = 'workshop_participants';
+
+    protected $fillable = [
+        'name',
+        'occupation',
+        'email',
+        'workshop_id',
+        'booking_transaction_id'
+    ];
+
+    public function workshop(): BelongsTo
+    {
+        return $this->belongsTo(workshop::class, 'workshop_id');
+    }
+
+
+    public function bookingTransaction(): BelongsTo
+    {
+        return $this->belongsTo(BookingTransaction::class, 'booking_transaction_id');
+    }
+}
